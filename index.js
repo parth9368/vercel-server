3b1463cd91d9606ad27f1119fe33e5e7da2cf235
Parth Patel <parthm9368@gmail.com>
2023-07-17 14:09:45 +0530

    some changes in code

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require("express")
+import express from "express";
+// const express = require("express")
 
 const app = express();
 
-const PORT = 5000;
+const PORT = 9000;
 
 app.use("/", (req,res) => {
     res.json({ message: "Hello from Express App"})
 });
 
-app.listen(5000, () => {
-    console.log(`starting server on port : ${PORT}`)
-})
+app.listen(9000, () => {
+    console.log(`starting server on port  ${PORT}`)
+});
 

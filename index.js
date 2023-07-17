103475fc4bf1bbec7e63d2ddae11f9d5375006ce
Prince Patel <pri2232@outlook.com>
2023-07-17 14:29:57 +0530

    Fix Some Code

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import express from "express";
-// const express = require("express")
 
 const app = express();
 
 const PORT = 9000;
 
-app.use("/", (req,res) => {
-    res.json({ message: "Hello from Express App"})
+app.use("/", (req, res) => {
+  res.json({ message: "Hello from Express App" });
 });
 
 app.listen(9000, () => {
-    console.log(`starting server on port  ${PORT}`)
+  console.log(`starting server on port  ${PORT}`);
 });
-
